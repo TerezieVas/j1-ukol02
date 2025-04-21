216de5073448ba34e78422278fb0e564ef602b93
Terezie Vašíčková <terezie.vasickova@gmail.com>
2025-04-21 16:35:16 +0200

    Trojúhelník

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -9,8 +9,23 @@ public class HlavniProgram {
         new HlavniProgram().start();
     }
 
+    public void trojuhelnik (double zakladna, double uhelB, double strana) {
+        zofka.turnRight(90);
+        zofka.move(zakladna);
+        zofka.turnLeft(180-uhelB);
+        zofka.move(strana);
+        double uhelC = (180-(uhelB*2));
+        zofka.turnLeft(180-uhelC);
+        zofka.move(strana);
+        zofka.turnLeft(180-uhelB);
+        zofka.turnLeft(90);
+    }
+
+
+
     public void start() {
         //TODO Tady bude kód pro kreslení želví grafiky.
+        trojuhelnik(50, 70, 70);
     }
 
 }
