01588975e3f62aeeb05cfa0290a4e207cb9171c9
Terezie Vašíčková <terezie.vasickova@gmail.com>
2025-04-21 17:32:58 +0200

    zmrzlina

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,6 +2,8 @@ package cz.czechitas.turtle;
 
 import dev.czechitas.java1.turtle.engine.Turtle;
 
+import java.awt.*;
+
 public class HlavniProgram {
     private Turtle zofka = new Turtle();
 
@@ -9,7 +11,8 @@ public class HlavniProgram {
         new HlavniProgram().start();
     }
 
-    public void trojuhelnik (double zakladna, double uhelB, double strana) {
+    public void trojuhelnik (double zakladna, double uhelB, double strana, Color barvaCary) {
+        zofka.setPenColor(barvaCary);
         zofka.turnRight(90);
         zofka.move(zakladna);
         zofka.turnLeft(180-uhelB);
@@ -20,7 +23,8 @@ public class HlavniProgram {
         zofka.turnLeft(180-uhelB);
         zofka.turnLeft(90);
     }
-    public void ctverec (double stranaCt){
+    public void ctverec (double stranaCt, Color barvaCary){
+        zofka.setPenColor(barvaCary);
         zofka.turnRight(90);
         for (int i=0; i<4; i++) {
             zofka.move(stranaCt);
@@ -29,7 +33,8 @@ public class HlavniProgram {
         zofka.turnLeft(90);
     }
 
-    public void obdelnik (double stranaOA, double stranaOB) {
+    public void obdelnik (double stranaOA, double stranaOB, Color barvaCary) {
+        zofka.setPenColor(barvaCary);
         zofka.turnRight(90);
         for (int i=0; i<2; i++) {
             zofka.move(stranaOA);
@@ -40,7 +45,8 @@ public class HlavniProgram {
         zofka.turnLeft(90);
     }
 
-    public void kolecko (double stranaK) {
+    public void kolecko (double stranaK, Color barvaCary) {
+        zofka.setPenColor(barvaCary);
         zofka.turnRight(90);
         for (int i=0; i<12; i++) {
             zofka.move(stranaK);
@@ -50,10 +56,24 @@ public class HlavniProgram {
     }
     public void start() {
         //TODO Tady bude kód pro kreslení želví grafiky.
-//        trojuhelnik(50, 70, 70);
-//        ctverec(50);
-//        obdelnik(50, 70);
-        kolecko(40);
+Color cervenaBarva;
+cervenaBarva = new Color(250,0,50);
+Color oranzovaBarva;
+oranzovaBarva = new Color(200, 100, 0);
+
+        zofka.turnLeft(180);
+
+        trojuhelnik(100,70,150,oranzovaBarva);
+
+        zofka.penUp();
+        zofka.turnRight(90);
+        zofka.move(65);
+        zofka.turnLeft(90);
+        zofka.move(35);
+        zofka.turnLeft(180);
+        zofka.penDown();
+        kolecko(28, cervenaBarva);
+
     }
 
 }
